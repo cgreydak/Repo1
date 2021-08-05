d368242d6d372de066701f8820e5ff03c7cea321
cgreydak <cgreydak@cisco.com>
2021-08-05 00:53:47 -0400

    Loop2.py

---
+++ Loop2.py
@@ -0,0 +1,12 @@
+#loop to 10
+#this is change on 8/5/2021 at 12:38
+count=1
+while count <= 10:
+ print(count)
+ count+=1
+#loop to 10
+#this is change on 8/5/2021 at 12:38
+count=-11
+while count <= 0:
+ print(count)
+ count+=1
